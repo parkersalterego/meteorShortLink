4111a21ec1a718648f5a0c677750218f82f54e61
parkersalterego <parkersalterego92@gmail.com>
2018-07-30 22:53:53 -0700

    routes sperated to own file and state used to trigger redirect component and programmatically navigate

--- imports/ui/Link.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class Link extends React.Component {
-    render(props) {
-        console.log(props);
-        return <p>This is the link component</p>
-    }
-}
