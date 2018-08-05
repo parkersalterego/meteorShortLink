953376ecb46e08e85279d1d5d3a9ae8f7a1ccfdc
parkersalterego <parkersalterego92@gmail.com>
2018-08-05 15:06:14 -0700

    lastVisitedAt and visitedCount added to links and momentjs pulled in for date formating

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -2,6 +2,7 @@ import Meteor from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
 
@@ -28,12 +29,26 @@ export default class LinksListItem extends React.Component {
         this.clipboard.destroy();
     }
 
+    renderStats() {
+        const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+        let visitedMessage = null;
+
+        if (typeof(this.props.lastVisitedAt) === 'number') {
+            visitedMessage = `(visited ${moment(this.props.lastVisitedAt).fromNow()})`;
+        }
+
+        return (
+            <p>{this.props.visitedCount} {visitMessage} - {visitedMessage}</p>
+        )
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
                 <p>{this.props.visable.toString()}</p>
+                {this.renderStats()}
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                     {this.state.justCoppied ? 'Copied' : 'Copy'}
                 </button>
@@ -53,5 +68,6 @@ LinksListItem.propTypes = {
     userId: PropTypes.string.isRequired,
     shortUrl: PropTypes.string.isRequired,
     visable: PropTypes.bool.isRequired,
-
+    visitedCount: PropTypes.number.isRequired,
+    lastVisitedAt: PropTypes.number,
 };
--- server/main.js
+++ server/main.js
@@ -1,12 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import  { WebApp } from 'meteor/webapp';
-
 import { Links } from '../imports/api/Links';
+import moment from 'moment';
 
 import '../imports/api/users';
 import '../imports/api/Links';
 
 Meteor.startup(() => {
+  let momentNow = moment();
+  console.log(momentNow.format('h:mma'));
+  
+
   WebApp.connectHandlers.use((req, res, next) => {
     const _id = req.url.slice(1);
     const link = Links.findOne({ _id });
@@ -14,7 +18,8 @@ Meteor.startup(() => {
     link 
       ? (res.statusCode = 302,
         res.setHeader('Location', link.url),
-        res.end())
+        res.end(), 
+        Meteor.call('links.trackVisit', _id))
       : next();
   });
 
